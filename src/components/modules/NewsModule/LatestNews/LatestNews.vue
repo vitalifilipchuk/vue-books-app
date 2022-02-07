73c4379988bb1f4e432d7bd82e1a31c73bdd45e9
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2022-02-08 00:37:01 +0200

    continue front page and news module

--- src/components/modules/NewsModule/LatestNews/LatestNews.vue
+++ src/components/modules/NewsModule/LatestNews/LatestNews.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="latest-news">
-        <template>
+        <template v-for="item in news">
+            <div class="latest-news__item" :key="item.id">
+                <div class="latest-news__item-title">
+                    {{item.title}}
+                </div>
+                <div class="latest-news__item-description">
+                    {{item.preview_text}}
+                </div>
+            </div>
         </template>
     </div>
 </template>
@@ -13,13 +21,22 @@ export default {
     props: {
         count: {
             type: Number,
-            default: 5,
+            default: 3,
         }
+    },
+
+    mounted() {
+        this.$store.dispatch('fetchNews').then((response) => {
+            let newsList = this.$store.getters.getNews
+            this.news = newsList.sort(function(a,b) {
+                return b.id - a.id
+            }).slice(0, this.count)
+        })
     },
 
     data() {
         return {
-
+            news: [],
         }
     },
 }
